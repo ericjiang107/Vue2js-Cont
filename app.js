72b14bc871b28fce7c42bcea95e13d83332f7481
Eric Jiang <ericjiang107@gmail.com>
2021-12-22 18:08:04 -0500

    loops

---
+++ app.js
@@ -0,0 +1,18 @@
+var instance1 = new Vue ({
+    el: '#v-for',
+    data: {
+        name: 'jack',
+        characters: ['Mario', "Luigi", "Yoshi", "Bowser"],
+        ninjas: [
+             { name: 'Eric', age: 25 },
+             { name: 'Tester', age: 25 },
+             { name: 'Ken', age: 55 }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
